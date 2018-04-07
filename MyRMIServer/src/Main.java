b1b8df8c67ae643d2d713083b4c8a4b5a3f1cea6
Wojtek Okoński <wojteko22.96@gmail.com>
2018-04-07 12:24:55 +0200

    Delete unnecessary files

--- MyRMIServer/src/Main.java
+++ MyRMIServer/src/Main.java
@@ -2,11 +2,31 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.util.Random;
 
-public class Farmer {
-    public static void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
-        checkArgs(args);
+public class Main {
+    public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
+        startWorkers();
+        startFarmer();
+    }
+
+    private static void startWorkers() throws RemoteException, MalformedURLException {
+        LocateRegistry.createRegistry(1099);
+        String[] addresses = {"//localhost/a", "//localhost/b"};
+        for (String address : addresses) {
+            try {
+                Worker worker = new WorkerImpl();
+                Naming.rebind(address, worker);
+                System.out.println("Server " + address + " is registered now :-)");
+            } catch (Exception e) {
+                System.out.println("SERVER CAN'T BE REGISTERED!");
+                throw e;
+            }
+        }
+    }
+
+    private static void startFarmer() throws RemoteException, MalformedURLException, NotBoundException {
         String[] addresses = {"//localhost/a", "//localhost/b"};
         for (String address : addresses) {
             Task task = new TaskImpl(new Random().nextInt());
@@ -19,13 +39,6 @@ public class Farmer {
                 throw e;
             }
             System.out.println("Result = " + result.getResult() + " " + result.getResult_description());
-        }
-    }
-
-    private static void checkArgs(String[] args) {
-        if (args.length != 1) {
-            throw new IllegalArgumentException(
-                    "You have to enter one RMI object address in the form: //host_address/service_name ");
         }
     }
 
--- MyRmiClient/src/CalcObject2.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface CalcObject2 extends Remote {
-    ResultType calculate(InputType inputParam) throws RemoteException;
-}
